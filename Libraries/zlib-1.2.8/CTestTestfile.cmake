ffdcfb72c13bc66e066561701810ccb0067ebb74
Jérôme <Jrme@P182>
2017-03-09 21:39:58 +0100

    Removed useless library files.

--- Libraries/zlib-1.2.8/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: E:/Workspace/MusicTools/Libraries/temp/zlib-1.2.8
-# Build directory: E:/Workspace/MusicTools/Libraries/temp/zlib-1.2.8
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(example "example")
